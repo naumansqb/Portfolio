515a609602b2f642532b23e64065349919999d25
Nauman <naumansqb@gmail.com>
2025-10-30 15:31:06 -0400

    Location change

--- src/main/java/com/pluralsight/Interface/Valuable.java
+++ src/main/java/com/pluralsight/Interface/Valuable.java
@@ -1,4 +1,4 @@
-package com.pluralsight;
+package com.pluralsight.Interface;
 
 public interface Valuable {
     double getValue();
--- src/main/java/com/pluralsight/assets/FixedAsset.java
+++ src/main/java/com/pluralsight/assets/FixedAsset.java
@@ -1,4 +1,6 @@
-package com.pluralsight;
+package com.pluralsight.assets;
+
+import com.pluralsight.Interface.Valuable;
 
 public abstract class FixedAsset implements Valuable {
     private String name;
@@ -6,18 +8,6 @@ public abstract class FixedAsset implements Valuable {
 
     public FixedAsset(String name, double value) {
         this.name = name;
-        this.value = value;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setValue(double value) {
         this.value = value;
     }
 
